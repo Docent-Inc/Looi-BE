bead16ca731e0575fd501a7dce088e25dd1a9a8a
“ssohye” <ssohye@icloud.com>
2023-03-29 15:52:11 +0900

    tnwjd

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
+
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import StreamingResponse
 from fastapi import Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 templates = Jinja2Templates(directory="templates")
@@ -14,6 +9,7 @@ templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("generic.html",{"request":request})
+    return templates.TemplateResponse("index.html", {"request": request})
