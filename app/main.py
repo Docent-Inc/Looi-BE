e86333da6b89e985c52d1b135feab8c1184ab770
taewan <taewan2002@naver.com>
2023-03-29 14:30:58 +0900

    [fix] html 이미지 안불러와지는 오류 수정

--- app/main.py
+++ app/main.py
@@ -2,10 +2,17 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 templates = Jinja2Templates(directory="templates")
 
-app = FastAPI(docs_url="/documentation", redoc_url=None)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def home(request: Request):
