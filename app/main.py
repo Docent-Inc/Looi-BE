29f3f902d8114482b6acf3eeae288bd811c4bca5
taewan <taewan2002@naver.com>
2023-04-03 22:16:35 +0900

    [create] chatgpt api제작

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from typing import Optional
 from fastapi.staticfiles import StaticFiles
 
 templates = Jinja2Templates(directory="templates")
@@ -9,7 +9,15 @@ templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+class GPTResponse(BaseModel):
+    text: str
+@app.get("/gpt/{text}", response_model=GPTResponse)
+async def get_gpt_result(text: str) -> GPTResponse:
+    generated_text = generate_text(text)
+    return GPTResponse(text=generated_text)
+
+
+
