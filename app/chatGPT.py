2f17091258de5df9d89ced9e3bcd62f7bdf22b23
taewan <taewan2002@naver.com>
2023-04-05 02:38:25 +0900

    [create] chatgpt api제작

--- app/chatGPT.py
+++ app/chatGPT.py
@@ -8,17 +8,18 @@ from datetime import datetime
 with open("app/gptkey.txt", "r") as f:
     openai.api_key = f.read().rstrip()
 
-messages = [
-    {"role": "system", "content": "당신은 내 조각난 꿈을 완성시켜줄거야. 나 대신에 꿈일기를 약간의 스토리텔링을 통해 한국어로 만들어줄거고, dalle2프롬프트도 만들어줄거야, 근데 그 프롬프트 명령어는 영어로 만들어줘"},
-    {"role": "system", "content": "dalle2그림을 illustration로 만들어줄거야"},
-    {"role": "system", "content": "꿈 일기 이름은 너가 정해줘"},
-    {"role": "system", "content": "DALLE-2 프롬프트 (영어): 이후에 프롬프트 작성해줘"},
-    {"role": "system", "content": "내용이 짧으면 약간의 추가적인 내용도 만들어줘"},
-    {"role": "system", "content": "맨 마지막 부분에 간단한 꿈 해몽 내용도 넣어줘"},
-    {"role": "system", "content": "꿈 일기 이름, 꿈 해몽, DALLE-2 프롬프트 (영어). 이 세게의 문단으로 구성되어야 해"},
-]
 def generate_text(text: str):
     try:
+        messages = [
+            {"role": "system",
+             "content": "당신은 내 조각난 꿈을 완성시켜줄거야. 나 대신에 꿈일기를 약간의 스토리텔링을 통해 한국어로 만들어줄거고, dalle2프롬프트도 만들어줄거야, 근데 그 프롬프트 명령어는 영어로 만들어줘"},
+            {"role": "system", "content": "dalle2그림을 illustration로 만들어줄거야"},
+            {"role": "system", "content": "꿈 일기 이름은 너가 정해줘"},
+            {"role": "system", "content": "DALLE-2 프롬프트 (영어): 이후에 프롬프트 작성해줘"},
+            {"role": "system", "content": "내용이 짧으면 약간의 추가적인 내용도 만들어줘"},
+            {"role": "system", "content": "맨 마지막 부분에 간단한 꿈 해몽 내용도 넣어줘"},
+            {"role": "system", "content": "꿈 일기 이름, 꿈 해몽, DALLE-2 프롬프트 (영어). 이 세게의 문단으로 구성되어야 해"},
+        ]
         messages.append({"role": "user", "content": text})
         if messages:
             chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)  # gpt-3.5-turbo
