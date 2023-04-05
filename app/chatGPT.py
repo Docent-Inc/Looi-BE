b37f3bbbdd2e04d67e5b525ed37ca74df6a83372
taewan <taewan2002@naver.com>
2023-04-06 01:37:26 +0900

    [fix] api 리턴형식 수정

--- app/chatGPT.py
+++ app/chatGPT.py
@@ -15,7 +15,7 @@ def generate_text(text: str):
         {"role": "system", "content": "당신은 내 조각난 꿈을 완성시켜줄거야. 나 대신에 꿈을 약간의 스토리텔링을 통해 한국어로 만들어줄거고, dalle2프롬프트도 만들어줄거야, 근데 그 프롬프트 명령어는 영어로 만들어줘"},
         {"role": "system", "content": "dalle2프롬프트에 illustration라는 말을 추가해서 만들어줄거야"},  # illustration, digital image
         {"role": "system", "content": "꿈 제목은 창의적인 제목으로 너가 정해줘"},
-        {"role": "system", "content": "꿈 제목(한글):, 꿈(한글):, 꿈 해몽(한글):, DALLE-2 프롬프트(영어): 이 네게의 문단으로 구성되어야 해, 각 문단의 이름이야"},
+        {"role": "system", "content": "[꿈 제목(한글)]:, [꿈(한글)]:, [꿈 해몽(한글)]:, [DALLE-2 프롬프트(영어)]: 이 네게의 문단으로 구성되어야 해, 각 문단의 이름이야"},
         {"role": "system", "content": "만약 내용이 짧으면 약 250자 정도까지 되도록 추가적인 스토리도 만들어줘"},
         {"role": "system", "content": "꿈 이후에 간단한 해몽 내용도 넣어줘, 해몽은 약 200자 정도로 만들어줘"},
         {"role": "system", "content": "꿈 내용은 1인칭 시점으로 작성해줘"},
@@ -26,7 +26,7 @@ def generate_text(text: str):
             chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)  # gpt-3.5-turbo
         reply = chat.choices[0].message.content
 
-        sections = ['꿈 제목(한글)', '꿈(한글)', '꿈 해몽(한글)', 'DALLE-2 프롬프트(영어)']
+        sections = ['[꿈 제목(한글)]', '[꿈(한글)]', '[꿈 해몽(한글)]', '[DALLE-2 프롬프트(영어)]']
         sliced_data = {}
 
         for i, section in enumerate(sections):
@@ -38,7 +38,7 @@ def generate_text(text: str):
                 sliced_data[section] = reply[start:].strip()
 
         response = openai.Image.create(
-            prompt=sliced_data['DALLE-2 프롬프트(영어)'],
+            prompt=sliced_data['[DALLE-2 프롬프트(영어)]'],
             n=1,  # 생성할 이미지 수
             size="512x512",  # 이미지 크기
             response_format="url"  # 응답 형식
@@ -55,12 +55,12 @@ def generate_text(text: str):
         picture = buffer.getvalue()
 
         # DB에 저장
-        dream = sliced_data['꿈(한글)']
-        dream_resolution = sliced_data['꿈 해몽(한글)']
+        dream = sliced_data['[꿈(한글)]']
+        dream_resolution = sliced_data['[꿈 해몽(한글)]']
 
         save_to_db(text, dream, dream_resolution, picture)
 
-        dream_name = sliced_data['꿈 제목(한글)']
+        dream_name = sliced_data['[꿈 제목(한글)]']
 
     except KeyboardInterrupt:
         print("Goodbye!")
