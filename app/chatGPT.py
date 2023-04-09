1ecc3f156070e676b71aa4385beeb821e9ece919
taewan <taewan2002@naver.com>
2023-04-09 17:36:21 +0900

    [fix] chatgpt api 호출 총 3번 이후 오류 발생

--- app/chatGPT.py
+++ app/chatGPT.py
@@ -15,11 +15,26 @@ async def generate_text(text: str):
     start_time = time.time()  # 실행 시작 시간 기록
     L = []
 
-    async def get_time(name):
+    async def get_time(name, start_time):
         print(f"{name} took {time.time() - start_time} seconds")
+
+    async def send_gpt_request(messages_prompt, retries=3):
+        for i in range(retries):
+            try:
+                chat = openai.ChatCompletion.create(model="gpt-4", messages=messages_prompt)
+                return chat.choices[0].message.content
+            except Exception as e:
+                print(e)
+                if i < retries - 1:
+                    print(f"Retrying {i + 1} of {retries}...")
+                else:
+                    print("Failed to get response after maximum retries")
+                    return "ERROR"
 
     async def get_gpt_response_and_more(message: str):
         dream = await get_gpt_response(message)
+        if dream == "ERROR":
+            return "ERROR"
         dream_name = dream[dream.find("[") + 1:dream.find("]")]
         dream = dream[dream.find("]") + 1:]
         dream_resolution, today_luck = await asyncio.gather(
@@ -29,19 +44,15 @@ async def generate_text(text: str):
         return dream_name, dream, dream_resolution, today_luck
 
     async def get_gpt_response(message: str) -> str:
-        try:
-            messages_prompt = [
-                {"role": "system", "content": "당신은 내 조각난 꿈을 완성시켜줄거야. 나 대신에 꿈을 약간의 스토리텔링을 통해 한국어로 만들어줄거야"},
-                {"role": "system",
-                 "content": "꿈 제목은 창의적인 제목으로 너가 정해주고, 꿈 내용은 1인칭 시점으로 작성해줘, 만약 내용이 짧으면 추가적인 내용을 만들어줘"},
-                {"role": "system", "content": "꿈 내용은 130자가 넘지 않도록 만들어줘"},
-                {"role": "system", "content": "꿈 제목은 []로 감싸주고 이어서 내용을 만들어줘"}, {"role": "user", "content": message}]
-            chat = openai.ChatCompletion.create(model="gpt-4", messages=messages_prompt)
-        except Exception as e:
-            print(e)
-            return get_gpt_response(message)
-        await get_time("get_gpt_response")
-        return chat.choices[0].message.content
+        messages_prompt = [
+            {"role": "system", "content": "당신은 내 조각난 꿈을 완성시켜줄거야. 나 대신에 꿈을 약간의 스토리텔링을 통해 한국어로 만들어줄거야"},
+            {"role": "system",
+             "content": "꿈 제목은 창의적인 제목으로 너가 정해주고, 꿈 내용은 1인칭 시점으로 작성해줘, 만약 내용이 짧으면 추가적인 내용을 만들어줘"},
+            {"role": "system", "content": "꿈 내용은 130자가 넘지 않도록 만들어줘"},
+            {"role": "system", "content": "꿈 제목은 []로 감싸주고 이어서 내용을 만들어줘"}, {"role": "user", "content": message}]
+        response = await send_gpt_request(messages_prompt)
+        await get_time("get_gpt_response", start_time)
+        return response
 
     async def get_image_url(prompt):
         response = await asyncio.to_thread(
@@ -61,35 +72,26 @@ async def generate_text(text: str):
         return buffer.getvalue()
 
     async def get_dream_resolution(message: str) -> str:
-        try:
-            messages_prompt = [
-                {"role": "system", "content": message},
-                {"role": "system", "content": "꿈 내용을 바탕으로 꿈 해몽을 만들어줘"},
-                {"role": "system", "content": "꿈 해몽은 80자가 넘지 않도록 해줘"},
-            ]
-            chat = openai.ChatCompletion.create(model="gpt-4", messages=messages_prompt)
-        except Exception as e:
-            print(e)
-            return get_dream_resolution(message)
-        await get_time("get_dream_resolution")
-        return chat.choices[0].message.content
+        messages_prompt = [
+            {"role": "system", "content": message},
+            {"role": "system", "content": "꿈 내용을 바탕으로 꿈 해몽을 만들어줘"},
+            {"role": "system", "content": "꿈 해몽은 80자가 넘지 않도록 해줘"},
+        ]
+        response = await send_gpt_request(messages_prompt)
+        await get_time("get_dream_resolution", start_time)
+        return response
 
     async def get_today_luck(message: str) -> str:
-        try:
-            messages_prompt = [
-                {"role": "system", "content": message},
-                {"role": "system", "content": "꿈을 바탕으로 오늘의 운세를 만들어줘"},
-                {"role": "system", "content": "오늘의 운세는 40자가 넘지 않도록 해줘"},
-                {"role": "system", "content": "제목없이 내용만 만들어줘, 현실을 반영해서 조심해야될 것 또는 좋은 일이 일어날 것 또는 꿈을 바탕으로 해야될 일을 적어줘"},
-                {"role": "system", "content": "오늘은으로 시작해줘"},
-            ]
-            chat = openai.ChatCompletion.create(model="gpt-4", messages=messages_prompt)
-        except Exception as e:
-            print(e)
-            return get_today_luck(message)
-        await get_time("get_today_luck")
-        return chat.choices[0].message.content
-
+        messages_prompt = [
+            {"role": "system", "content": message},
+            {"role": "system", "content": "꿈을 바탕으로 오늘의 운세를 만들어줘"},
+            {"role": "system", "content": "오늘의 운세는 40자가 넘지 않도록 해줘"},
+            {"role": "system", "content": "제목없이 내용만 만들어줘, 현실을 반영해서 조심해야될 것 또는 좋은 일이 일어날 것 또는 꿈을 바탕으로 해야될 일을 적어줘"},
+            {"role": "system", "content": "오늘은으로 시작해줘"},
+        ]
+        response = await send_gpt_request(messages_prompt)
+        await get_time("get_today_luck", start_time)
+        return response
     async def DALLE2(message: str) -> str:
         try:
             messages_prompt = [
@@ -99,10 +101,10 @@ async def generate_text(text: str):
             chat = openai.ChatCompletion.create(model="gpt-4", messages=messages_prompt)
         except Exception as e:
             print(e)
-            return "error"
+            return "OpenAI API Error"
         dream_image_url = await get_image_url(chat.choices[0].message.content)
         dream_image = await download_image(dream_image_url)
-        await get_time("DALLE2")
+        await get_time("DALLE2", start_time)
         return [dream_image, dream_image_url]
 
     results, L = await asyncio.gather(
@@ -113,7 +115,7 @@ async def generate_text(text: str):
 
     save_to_db(text, dream_name + dream, dream_resolution + today_luck, L[0])
 
-    await get_time("total")
+    await get_time("total", start_time)
 
     return dream_name, dream, dream_resolution, today_luck, L[1]
 
