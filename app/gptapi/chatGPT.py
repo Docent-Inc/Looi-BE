c784453e981f3f7d6f5479d2430ab2eb855bce7a
taewan <taewan2002@naver.com>
2023-04-12 17:29:09 +0900

    [fix] 빌런 처치

--- app/gptapi/chatGPT.py
+++ app/gptapi/chatGPT.py
@@ -12,6 +12,9 @@ with open("app/gptapi/gptkey.txt", "r") as f:
     openai.api_key = f.read().rstrip()
 
 async def generate_text(text: str) -> str:
+    if text.find("성한빈") or text.find("한빈") or text.find("장하오"):
+        return "그만해", "너 IP다 털렸다.", "수고해", "경찰서에서 보자", "https://www.police.go.kr/index.do"
+
     start_time = time.time()  # 실행 시작 시간 기록
     L = []
 
