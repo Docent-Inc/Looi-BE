b332745db4a6eadf02fab020c2ac0354533f6cd2
taewan <taewan2002@naver.com>
2023-04-12 15:10:38 +0900

    [create] 쿠키 로그 저장 기능 추가.

--- app/routers/gpt.py
+++ app/routers/gpt.py
@@ -4,7 +4,9 @@ from app.schemas.gpt import GPTResponse
 from app.schemas.common import ApiResponse
 from pydantic import BaseModel
 from fastapi import Request
-
+from fastapi import Cookie, Response
+from typing import Optional
+import uuid
 
 
 from sqlalchemy import create_engine
@@ -17,10 +19,13 @@ Base = declarative_base()
 
 router = APIRouter(prefix="/gpt")
 @router.get("/{text}", response_model=ApiResponse, tags=["gpt"])
-async def get_gpt_result(request: Request, text: str) -> GPTResponse:
-    dream_name, dream, dream_resolution, today_luck, dream_image_url = await generate_text(text, request.client.host)
+async def get_gpt_result(text: str, user_cookie: Optional[str] = Cookie(None)) -> GPTResponse:
+    dream_name, dream, dream_resolution, today_luck, dream_image_url = await generate_text(text, user_cookie)
 
-    return ApiResponse(
+    if user_cookie is None:
+        user_cookie = str(uuid.uuid4())  # 새로운 고유한 ID 생성
+
+    response = ApiResponse(
         success=True,
         data=GPTResponse(
             dream_name=dream_name,
@@ -30,6 +35,11 @@ async def get_gpt_result(request: Request, text: str) -> GPTResponse:
             image_url=dream_image_url
         )
     )
+
+    if not user_cookie:
+        response.set_cookie(key="user_cookie", value=user_cookie, max_age=60 * 60 * 24 * 30)  # 쿠키를 30일 동안 유지
+
+    return response
 
 class PhoneNumberData(BaseModel):
     phoneNumber: str
