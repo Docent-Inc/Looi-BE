bbbfba323f63b7d889c324d59bcc03395c958742
taewan <taewan2002@naver.com>
2023-04-12 13:18:24 +0900

    [create] 사용자의 ip정보 db에 추가저장

--- app/models/dream.py
+++ app/models/dream.py
@@ -1,11 +1,21 @@
 from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.dialects.mysql import LONGBLOB
 from app.db.database import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DB_URL = 'mysql+pymysql://dmz:1234@swiftsjh.tplinkdns.com:3306/BMSM'
+engine = create_engine(DB_URL)
+
+Base = declarative_base()
+
 class Dream(Base):
-    __tablename__ = "dreams"
+    __tablename__ = "dreams_pretotype"
 
     id = Column(Integer, primary_key=True)
     text = Column(Text)
     dream_name = Column(Text)
     dream_resolution = Column(Text)
-    image_url = Column(LONGBLOB)
+    # image_url = Column(LONGBLOB)
+
+Base.metadata.create_all(bind=engine)
