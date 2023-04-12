fd415fbb6a1a96d7f037a26b455d63d5c4567579
taewan <taewan2002@naver.com>
2023-04-13 05:08:43 +0900

    [create] pretotyping data 수집 로직 추가

--- app/routers/gpt.py
+++ app/routers/gpt.py
@@ -17,7 +17,7 @@ router = APIRouter(prefix="/gpt")
 
 @router.post("/", response_model=ApiResponse, tags=["gpt"])
 async def get_gpt_result(survey_data: SurveyData = Body()) -> GPTResponse:
-    dream_name, dream, dream_resolution, today_luck, dream_image_url = await generate_text(survey_data.text, survey_data)
+    dream_name, dream, dream_resolution, today_luck, dream_image_url = await generate_text(survey_data.dream, survey_data)
 
     return ApiResponse(
         success=True,
