2d4b8cd1220d65a831cc983920a80d7b998485ef
taewan <taewan2002@naver.com>
2023-04-18 15:44:26 +0900

    db foreign 키 설정

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,6 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.declarative import declarative_base
-
 from app.db.models.dream import get_DreamBase
-from app.db.models.user import get_UserBase
 
 PUBLIC_IP_ADDRESS = '34.64.33.205' # gcp sdl database
 DB_USER = 'docent'
@@ -14,14 +11,8 @@ DB_NAME = 'test'
 DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{PUBLIC_IP_ADDRESS}/{DB_NAME}'
 engine = create_engine(DB_URL, pool_recycle=500)
 
-# 각 Base의 메타데이터를 병합
-merged_metadata = MetaData()
-for base in [get_UserBase(), get_DreamBase()]:
-    for table in base.metadata.tables.values():
-        table.tometadata(merged_metadata)
-
-# 병합된 메타데이터로 새로운 Base 생성
-Base = declarative_base(metadata=merged_metadata)
+Base = get_DreamBase()
+# Base.metadata.drop_all(bind=engine) # 테이블 변경 사항 있을 시 주석 제거
 Base.metadata.create_all(bind=engine)  # 테이블 생성
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
