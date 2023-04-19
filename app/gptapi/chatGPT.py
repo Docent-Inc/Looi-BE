5fc3b2d40fa429707b32a69efbf82e8b4a64de2b
taewan <taewan2002@naver.com>
2023-04-19 14:19:13 +0900

    데이터베이스 저장 기능 비동기 작업

--- app/gptapi/chatGPT.py
+++ app/gptapi/chatGPT.py
@@ -1,44 +1,35 @@
 import openai
-import requests
-from PIL import Image
-from io import BytesIO
 import asyncio
 import time
 from app.schemas.survey import SurveyData
-from google.cloud import storage
-from google.oauth2 import service_account
-
-from app.db.dream import save_to_db
-
+from app.models.dream import Dream
+from app.db.database import SessionLocal
 # gptkey.txt 파일 읽어오기
 with open("app/gptapi/gptkey.txt", "r") as f:
     openai.api_key = f.read().rstrip()
 
-async def generate_text(text: str, survay_data: SurveyData) -> str:
-    if text.find("성한빈") != -1 or text.find("한빈") != -1 or text.find("장하오") != -1:
-        return "그만해", "너 IP다 털렸다.", "수고해", "경찰서에서 보자", "https://www.police.go.kr/index.do"
+async def save_to_db_async(text, dream, dream_resolution, survey_data):
+    def save_to_db(text, dream, dream_resolution, survey_data: SurveyData):
+        session = SessionLocal()
+        new_dream = Dream(
+            dreamTime=survey_data.dreamTime,
+            isRecordDream=survey_data.isRecordDream,
+            isShare=survey_data.isShare,
+            isRecordPlatform=survey_data.isRecordPlatform,
+            sex=survey_data.sex,
+            mbti=survey_data.mbti,
+            department=survey_data.department,
+            text=text,
+            dream_name=dream,
+            dream_resolution=dream_resolution
+        )  # image_url=image_url)
+        session.add(new_dream)
+        session.commit()
+        session.close()
+    save_to_db(text, dream, dream_resolution, survey_data)
 
-
+async def generate_text(text: str, survey_data: SurveyData) -> str:
     start_time = time.time()  # 실행 시작 시간 기록
-    L = []
-    SERVICE_ACCOUNT_INFO = {
-        "type": "service_account",
-        "project_id": "bold-landing-380312",
-        "private_key_id": "07d19af2d593c76000c947f9efc2f808a5dd526c",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDEiszeXaRxfJ7p\nNxgjqt2Lv08j/35u1ihwyMXIx8XUgcY+LgSMHzFRViq7JFc2C1cApHn1e4DV8GKY\nttyP5k8Tx/5v50EFCUG8soqQRLEOTW/0QMmoPRKiegPICg7BFvQ7KZP9tl1ptLUN\nEJG7AdIXnz49XM1FibJSefhzdfJK0IWS6t5pCihLhuf7e8y8QYkwh5i9AAedbSdO\ndb3D3XGBmLtvJdnDDbXcBRIw/rm4zSUInEW/hHfbZe+EV4miig/uHFjCd9GUkeQg\n/61wd8Gr0f8HNrEelQ3tDdZPRhVxYM7jyNrbezV02nDNPHLgIiihsHgz7xgv8Rid\n4zsHOKyBAgMBAAECggEACBEya4Yoc8gLtcLfKMegpFXL58xa4i3EJyz2gVFB24Eg\nI/k7kozNN2t0AY5yTfOVNJ+LqfnRxfZB9ca7suRfJo608N7rGkvQ+O/TJCzNn6qq\nB7qJDHDFTQn5EcLn7CEHEU6ZlnZfyzDxpYGimaxvdSzCHz6p5krKmPwMqKCUu2FV\nPfvnwzob/EDrGLH7SaFefsEIICmzzVKRlMiCCIHVYSihsUXl1JuulxxPvhwYKkaY\nRgPNQnXmhsjYl8DL7Tw/aHyJvKF/Oxu8RSO0Krip1es2tuuCHc1zuPP/bCgX1JVS\nGQOEodhbLnr8JNvlQK/v2oGA34crJaK9em7uMPoECQKBgQD07reDNT4qKfwpwQ6O\nVac4NJW05+XBy3Ep0pVEVHLk0+hANnY6eWCyv3zlhmsQUFM2oYepXFpM1ym4CxS0\nfBzf7N53DD5RCZIbioJ8U9XErDv2IJBjpp9RDxF3cH/YelDNyhHjbZsWLpWze06o\ngz1DMiF8fBpznnLncgpDgsPZHwKBgQDNbFLJG6L0lGsE4Q6mK4ldWByhrDJy1P6R\noDjm+Kj9c9Ocurk5aSCfHEF6qdS64jfVOHD+AQgxzas9spwhR+inRlaamxRmaNZZ\nih6pXOMHmgmyqqxLcMStOJyYt5yOXSmieOsm4hNqL8sOatvjbntmQAk8tt//qXxf\ngFEy6R+mXwKBgQCXQ549//HGZGuA9eOhb7B3+7HBKb4xMw1OQOlCa80RGPXEQl49\nupxHiA8ASUVxq8nYYWXA6HI0JmVzbhR5anUCreyuJPePYJPFQoNXeQb6EUxusqm5\ngTu++clVtDqgXNnuXa8yf4xZ5Kc7Uxm+5F4/U4Ruts43PVHFMh64lteRDQKBgClV\niZbj07dJAfu6WVtAWWSJ7UDuyDvo9cxRpAF9uWs+Wi8oN0sBB0pcwiQvdhmgmUFz\nGPFTPdXfn3xLqzTbJko6UgTL/Z/Zqn/b1e2YPipyaU8lHoQTjc+ZG5FzKLJQtqb1\nk8OALA3qzf35rIMn4PajHfi0h4AHF3qT9EK9O9wbAoGBAIAkcV4BkToqy5ISnAEW\nsr6jxi37GrT5mxSf9Xy/OjhVp1hqOROueRHAbFZkJrXUOD3G3dG53CsgvoUfZRGY\n+x0HG0M/2ZBkUmA556WLh8K4n4L/AKhYkHD/+qPp9DDnVwIpaDdihJh2M9wH8ehe\nCl2Guw8GfrtqdpPTfqVraOmM\n-----END PRIVATE KEY-----\n",
-        "client_email": "test-555@bold-landing-380312.iam.gserviceaccount.com",
-        "client_id": "113573215612941765368",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test-555%40bold-landing-380312.iam.gserviceaccount.com"
-    }
-    async def upload_image_to_gcs(client, bucket_name, image_data, destination_blob_name):
-        bucket = client.get_bucket(bucket_name)
-        blob = bucket.blob(destination_blob_name)
-
-        with BytesIO(image_data) as image_file:
-            blob.upload_from_file(image_file)
 
     async def get_time(name, start_time):
         print(f"{name} took {time.time() - start_time} seconds")
@@ -89,13 +80,6 @@ async def generate_text(text: str, survay_data: SurveyData) -> str:
         )
         return response['data'][0]['url']
 
-    async def download_image(url):
-        response = await asyncio.to_thread(requests.get, url)
-        img = Image.open(BytesIO(response.content))
-        buffer = BytesIO()
-        img.save(buffer, format="PNG")
-        return buffer.getvalue()
-
     async def get_dream_resolution(message: str) -> str:
         messages_prompt = [
             {"role": "system", "content": message},
@@ -128,30 +112,18 @@ async def generate_text(text: str, survay_data: SurveyData) -> str:
             print(e)
             return "OpenAI API Error"
         dream_image_url = await get_image_url(chat.choices[0].message.content)
-        # dream_image_data = await download_image(dream_image_url)
-        dream_image_data = " "
         await get_time("DALLE2", start_time)
-        return [dream_image_data, dream_image_url]
+        return dream_image_url
 
-    results, L = await asyncio.gather(
+    results, dream_image_url = await asyncio.gather(
         get_gpt_response_and_more(text),
         DALLE2(text)
     )
     dream_name, dream, dream_resolution, today_luck = results
-    dream_image_data, dream_image_url = L
-    destination_blob_name = dream_name + ".png"  # 원하는 파일명을 지정하세요.
-    bucket_name = "bmongsmongimg"  # 구글 클라우드 버킷 이름을 지정하세요.
 
-    def create_storage_client_hardcoded():
-        credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO)
-        return storage.Client(credentials=credentials, project=SERVICE_ACCOUNT_INFO['project_id'])
-
-    # client = create_storage_client_hardcoded()
-    # await upload_image_to_gcs(client, bucket_name, dream_image_data, destination_blob_name)
-
-    save_to_db(text, dream_name + dream, dream_resolution + today_luck, survay_data)
-
+    db_task = asyncio.create_task(
+        save_to_db_async(text, dream_name + dream, dream_resolution + today_luck, survey_data))
     await get_time("total", start_time)
 
-    return dream_name, dream, dream_resolution, today_luck, L[1]
+    return dream_name, dream, dream_resolution, today_luck, dream_image_url
 
