2d9ad391b5e8e49fc814e4df1f839a539582dcda
taewan <taewan2002@naver.com>
2023-05-01 22:52:22 +0900

    fix: Auth기능 HTTPException예외 처리
    데이터베이스에 저장하다가 실패 시 예외 코드 500 반환하도록 설정

--- app/auth/user.py
+++ app/auth/user.py
@@ -15,10 +15,16 @@ def create_user(db: Session, user: UserCreate) -> User:
         is_active=True,
         is_deleted=False
     )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="유저정보 생성을 실패했습니다.",  # 에러 메시지를 반환합니다.
+        )
 
 def authenticate_user(db: Session, email: str, password: str) -> Optional[User]:
     user = get_user_by_email(db, email) # 이메일로 사용자 정보 가져오기
@@ -43,7 +49,7 @@ def changeNickName(requset_nickName: str, current_user: User, db: Session):
         db.refresh(current_user)
     except:
         raise HTTPException(
-            status_code=status.HTTP_511_NETWORK_AUTHENTICATION_REQUIRED,
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="닉네임 변경이 실패했습니다.",  # 에러 메시지를 반환합니다.
         )
 
@@ -57,16 +63,28 @@ def changePassword(request_password: str, current_user: User, db: Session):
         )
     # 새로운 비밀번호를 해시합니다.
     current_user.hashed_password = get_password_hash(request_password)
-    db.add(current_user)
-    db.commit()
-    db.refresh(current_user)
+    try:
+        db.add(current_user)
+        db.commit()
+        db.refresh(current_user)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="비밀번호 변경을 실패했습니다.",  # 에러 메시지를 반환합니다.
+        )
 
 def deleteUser(current_user: User, db: Session):
     # 사용자의 삭제 상태를 변경합니다.
     current_user.is_deleted = True
-    db.add(current_user)
-    db.commit()
-    db.refresh(current_user)
+    try:
+        db.add(current_user)
+        db.commit()
+        db.refresh(current_user)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="유저정보 지우는것을 실패했습니다.",  # 에러 메시지를 반환합니다.
+        )
 
 def user_kakao(kakao_data: dict, db: Session) -> Optional[User]:
     # 카카오에서 전달받은 사용자 정보를 변수에 저장합니다.
@@ -82,7 +100,13 @@ def user_kakao(kakao_data: dict, db: Session) -> Optional[User]:
             nickName=kakao_nickname,
             hashed_password=get_password_hash(kakao_id),
         )
-        db.add(user)
-        db.commit()
-        db.refresh(user)
+        try:
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+        except:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="유저정보를 저장하는데 실패했습니다.",
+            )
     return user
