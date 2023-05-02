07ca58e97fa7e771cbb7ebba4630135086feb5a1
taewan <taewan2002@naver.com>
2023-05-02 11:14:14 +0900

    feat: 인기게시물 선정 로직 추가, 좋아요 기능
    좋아요 눌렀을 때 hot테이블에 데이터 추가 로직 구현

--- app/crud/diary.py
+++ app/crud/diary.py
@@ -1,6 +1,8 @@
 from app.core.current_time import get_current_time
+from app.crud.hot import maintain_hot_table_limit
 from app.db.database import get_db
 from app.db.models.diary import Diary
+from app.db.models.hot import Hot
 from app.db.models.like import Like
 from app.schemas.request.crud import Create, Update
 from fastapi import HTTPException
@@ -131,8 +133,34 @@ async def likeDiary(diaryId: int, userId: int, db: get_db()):
         db.add(like)
         db.commit()
         db.refresh(like)
-    except:
-        raise HTTPException(status_code=500, detail="데이터베이스에 오류가 발생했습니다.")
+
+        # 가장 오래된 데이터의 id를 사용하거나, 그렇지 않으면 가장 큰 id에 1을 더합니다.
+        # Hot 테이블에 추가하기 전에 데이터 제한을 확인하고 관리합니다.
+        oldest_index = await maintain_hot_table_limit(db)
+        if oldest_index is None:
+            first_hot = db.query(Hot).order_by(Hot.id.desc()).first()
+            if first_hot is None:
+                max_index = 0
+            else:
+                max_index = first_hot.index
+            new_index = max_index + 1
+        else:
+            new_index = oldest_index
+
+        # Hot 테이블에 가중치 추가
+        existing_hot = db.query(Hot).filter(Hot.Diary_id == diaryId, Hot.User_id == userId, Hot.weight == 3).first()
+        if existing_hot is None:
+            hot = Hot(
+                index=new_index,
+                weight=3,  # 좋아요 가중치
+                Diary_id=diaryId,
+                User_id=userId
+            )
+            db.add(hot)
+            db.commit()
+            db.refresh(hot)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 async def unlikeDiary(diaryId: int, userId: int, db: get_db()):
     diary = db.query(Diary).filter(Diary.id == diaryId).first()
