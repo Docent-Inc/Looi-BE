f73f0075ebf7bfc8b0cc306e5ae6508976d69548
taewan <taewan2002@naver.com>
2023-05-02 18:59:45 +0900

    fix: generate기능 맵핑 방식 변경
    post 맵핑에서 get 맵핑으로 변경

--- app/routers/generate.py
+++ app/routers/generate.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, Depends
-from app.feature.gptapi import generate_text
+from app.feature.gptapi.generateDream import generate_text
+from app.feature.gptapi.generateCheckList import generate_checklist
 from app.schemas.response.gpt import BasicResponse, ImageResponse, CheckListResponse
 from app.schemas.common import ApiResponse
 from app.feature.gptapi.generateImg import additional_generate_image
-from app.feature.gptapi import generate_checklist
 from app.core.security import get_current_user
 from app.schemas.response.user import User
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 router = APIRouter(prefix="/generate")
 
-@router.post("/dream", response_model=ApiResponse, tags=["Generate"])
+@router.get("/dream", response_model=ApiResponse, tags=["Generate"])
 async def generate_basic(
     text: str, # 사용자가 입력한 텍스트
     db: Session = Depends(get_db), # 데이터베이스 세션
@@ -26,7 +26,7 @@ async def generate_basic(
         )
     )
 
-@router.post("/image", response_model=ApiResponse, tags=["Generate"])
+@router.get("/image", response_model=ApiResponse, tags=["Generate"])
 async def generate_image(
     textId: int, # 생성된 꿈 텍스트의 id
     current_user: User = Depends(get_current_user),
@@ -40,7 +40,7 @@ async def generate_image(
         )
     )
 
-@router.post("/checklist", response_model=ApiResponse, tags=["Generate"])
+@router.get("/checklist", response_model=ApiResponse, tags=["Generate"])
 async def generate_image(
     textId: int, # 생성된 꿈 텍스트의 id
     current_user: User = Depends(get_current_user),
