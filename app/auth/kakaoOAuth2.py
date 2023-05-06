8fb8983b4a58c0a268e58eb54901ff66fa4abb34
taewan <taewan2002@naver.com>
2023-05-06 21:26:52 +0900

    fix: 카카오로그인 리다이렉트 url 수정

--- app/auth/kakaoOAuth2.py
+++ app/auth/kakaoOAuth2.py
@@ -7,8 +7,7 @@ import os
 load_dotenv()
 CLIENT_ID = os.getenv("KAKAO_API_KEY")
 CLIENT_SECRET = os.getenv("KAKAO_API_SECRET")
-# REDIRECT_URI = "https://bmongsmong.com/kakao"
-REDIRECT_URI = "http://localhost:3000/kakao"
+REDIRECT_URI = "https://bmongsmong.com/api/auth/kakao/callback"
 KAKAO_AUTH_URL = f"https://kauth.kakao.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code"
 AUTHORIZE_ENDPOINT = "https://kauth.kakao.com/oauth/authorize"
 ACCESS_TOKEN_ENDPOINT = "https://kauth.kakao.com/oauth/token"
