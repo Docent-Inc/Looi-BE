290d0e8713f7cc8d27312ce8abc7cf615b4fdd5d
taewan <taewan2002@naver.com>
2023-05-07 11:52:24 +0900

    fix: gpt 프롬프팅 수정

--- app/feature/gptapi/generateDream.py
+++ app/feature/gptapi/generateDream.py
@@ -8,7 +8,7 @@ from app.feature.gptapi.gptRequset import send_gpt_request
 async def generate_text(text: str, userId: int, db: get_db()) -> str:
     async def get_dreamName(message: str) -> str:
         messages_prompt = [
-            {"role": "system", "content": "Understand this dream, and make a interested dream title in just Korean."},
+            {"role": "system", "content": "꿈의 내용을 이해하고 너가 재미있는 꿈의 제목을 만들어줘"},
             {"role": "user", "content": message}
         ]
         dreamName = await send_gpt_request(messages_prompt)
