463b9dcd82a3d5f14d1cdadd4d9e1de54d4339cd
taewan <taewan2002@naver.com>
2023-05-14 16:30:58 +0900

    fix: 인기게시물 불러오는 api 기능 수정
    데이터베이스 오류 수정 및 최적화

--- app/routers/search.py
+++ app/routers/search.py
@@ -1,4 +1,4 @@
-from app.db.models.diary import Diary
+from app.db.models.user import User
 from app.feature.search import listHot, listText
 from app.schemas.common import ApiResponse
 from fastapi import APIRouter, Depends
@@ -7,7 +7,6 @@ from app.core.security import get_current_user
 from sqlalchemy.orm import Session
 
 from app.schemas.response.diary import DiaryListResponse
-from app.schemas.response.user import User
 router = APIRouter(prefix="/search")
 
 @router.get("/hot", response_model=ApiResponse, tags=["Search"])
@@ -16,20 +15,7 @@ async def get_hot(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    hot_list = await listHot(page, db)
-    diary_list_response = []
-    for hot_item in hot_list:
-        diary_id, total_weight = hot_item
-        diary = db.query(Diary).filter(Diary.id == diary_id, Diary.is_deleted == False).first()
-        diary_response = DiaryListResponse(
-            id=diary.id,
-            dream_name=diary.dream_name,
-            image_url=diary.image_url,
-            view_count=diary.view_count,
-            like_count=diary.like_count,
-            comment_count=diary.comment_count
-        )
-        diary_list_response.append(diary_response)
+    diary_list_response = await listHot(page, db, current_user)
 
     return ApiResponse(
         success=True,
