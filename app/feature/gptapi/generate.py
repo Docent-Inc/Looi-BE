c200c7fdacc0cd82941be541101ff547ea75ba08
taewan <taewan2002@naver.com>
2023-05-17 20:47:22 +0900

    fix: dalle2 프롬프팅 구체화

--- app/feature/gptapi/generate.py
+++ app/feature/gptapi/generate.py
@@ -22,7 +22,8 @@ async def generate_text(text: str, userId: int, db: get_db()) -> str:
     async def DALLE2(message: str):
         try:
             messages_prompt = [
-                {"role": "system", "content": "Understand this dream and make just one scene a prompt for DALLE2, include the word illustration"},
+                {"role": "system", "content": "Understand this dream and make just one scene a prompt for DALLE2"},
+                {"role": "system", "content": "include the word illustration and 7 world about Subject, Medium, Environment, Lighting, Color, Mood, Compoition"},
                 {"role": "system", "content": "make just prompt only engilsh"},
                 {"role": "system", "content": "max_length=100"},
                 {"role": "user", "content": message}
