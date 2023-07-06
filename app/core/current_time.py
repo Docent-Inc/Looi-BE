d59e29efda64dc3cf652842c2fbf1a3cafb9d4db
taewan <taewan2002@naver.com>
2023-07-06 21:57:15 +0900

    feat: Line 에코봇 테스트

--- app/core/current_time.py
+++ app/core/current_time.py
@@ -5,3 +5,9 @@ def get_current_time():
     korea_time = datetime.now(korea_timezone)
     formatted_time = korea_time.strftime("%Y%m%d%H%M%S")
     return formatted_time
+
+def get_jp_time():
+    jp_timezone = pytz.timezone("Asia/Tokyo")
+    jp_time = datetime.now(jp_timezone)
+    formatted_time = jp_time.strftime("%Y%m%d%H%M%S")
+    return formatted_time
