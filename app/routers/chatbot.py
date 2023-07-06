03fb2f86478b087343d7f73292d3e9eb4810da41
taewan <taewan2002@naver.com>
2023-07-06 14:46:17 +0900

    feat: Line 에코봇 테스트

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -29,18 +29,16 @@ router = APIRouter(prefix="/chatbot")
 @router.post("/callback")
 async def callback(request: Request, body: Optional[LineWebhookBody] = None):
     if body is None:
-        print("Request body is missing.")
         raise HTTPException(status_code=400, detail="Bad Request: Request body is missing.")
     signature = request.headers.get('X-Line-Signature')
     if not signature:
-        print("Signature is missing.")
         raise HTTPException(status_code=400, detail="Bad Request: Signature is missing.")
     try:
-        handler.handle(body.dict(), signature)
+        handler.handle(body.json(), signature)
     except InvalidSignatureError:
-        print("Invalid signature. Check your channel access token/channel secret.")
         raise HTTPException(status_code=400, detail="Invalid signature. Check your channel access token/channel secret.")
     return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
