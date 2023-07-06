38bafd312dda3b137e2abdb655428bb45883477c
taewan <taewan2002@naver.com>
2023-07-06 16:29:56 +0900

    feat: Line 에코봇 테스트

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -1,12 +1,30 @@
-import json
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage
 from fastapi import APIRouter, Request, HTTPException
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 from dotenv import load_dotenv
 import os
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from app import db
+from app.feature.diary import createDiary
+from app.feature.generate_jp import generate_text, generate_resolution_linechatbot
+from app.schemas.request.crud import Create
+
+# Initialize the scheduler
+scheduler = AsyncIOScheduler()
+user_requests = {}
+MAX_REQUESTS_PER_DAY = 3  # This can be any number you want
+# Define the function to reset the counter
+def reset_counter():
+    global user_requests
+    user_requests = {}
+
+# Schedule the function to run every day at midnight
+scheduler.add_job(reset_counter, 'cron', hour=0)
+scheduler.start()
 
 load_dotenv()
 
@@ -42,10 +60,48 @@ async def callback(request: Request):
     return 'OK'
 
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    dream_text = event.message.text
+
+    # 글자 수 제한
+    if len(dream_text) < 10 or len(dream_text) > 200:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="10文字以上200文字以下で入力してください。"))  # Echo message
+        return
+
+    # 꿈 생성 제한 3회
+    user_id = event.source.user_id
+    if user_requests[user_id] > MAX_REQUESTS_PER_DAY:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="1日3回までです。"))
+        return
+
+    # 꿈 생성
+    id, dream_name, dream, dream_image_url = generate_text(dream_text, 3, db)
+    # 해몽 생성
+    dream_resolution = generate_resolution_linechatbot(dream_text)
+
+    create = Create(
+        dream_name=dream_name,
+        dream=dream,
+        image_url=dream_image_url,
+        resolution=dream_resolution,
+        checklist="checklist",
+        is_public=True,
+    )
+    await createDiary(create, 3, db)
+    generated_text = f"【{dream_name}】\n{dream}\n\n【夢占い】\n{dream_resolution}"
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))  # Echo message
+        ImageSendMessage(original_content_url=dream_image_url, preview_image_url=dream_image_url),
+        TextSendMessage(text=generated_text)
+    )
 
+    if user_id not in user_requests:
+        user_requests[user_id] = 0
+    user_requests[user_id] += 1
+    return
