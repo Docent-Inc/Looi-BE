621a9e085504da2a28ad27f13d2e770612579667
taewan <taewan2002@naver.com>
2023-07-23 17:44:02 +0900

    feat: 카카오챗봇 user데이터베이스 추가

--- app/routers/kakao_chatbot.py
+++ app/routers/kakao_chatbot.py
@@ -1,13 +1,15 @@
 import asyncio
 from typing import Dict, Any
 import requests
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette.background import BackgroundTasks
 from app.db.database import get_db
+from app.db.models.kakao_chatbot_dream import kakao_chatbot_dream
+from app.db.models.kakao_chatbot_user import kakao_chatbot_user
 from app.feature.diary import createDiary
-from app.feature.generate_kr import generate_text, generate_resolution, generate_resolution_clova
+from app.feature.generate_kr import generate_text, generate_resolution_clova
 from app.schemas.response.kakao_chatbot import Output, SimpleImage, SimpleText, KakaoChatbotResponse, Template
 from app.schemas.request.crud import Create
 
@@ -17,15 +19,21 @@ from app.schemas.request.crud import Create
 
 router = APIRouter(prefix="/kakao-chatbot")
 
-# 매일 0시에 카운터 초기화
-scheduler = AsyncIOScheduler(timezone="Asia/Seoul")
-user_requests = {}
+# 매일 0시에 모든 user 의 day_count 를 0으로 초기화
 MAX_REQUESTS_PER_DAY = 3
-def reset_counter():
-    global user_requests
-    user_requests = {}
+async def reset_day_count(db: Session = Depends(get_db)):
+    users = db.query(kakao_chatbot_user).all()
+    for user in users:
+        user.day_count = 0
+    db.commit()
 
-scheduler.add_job(reset_counter, 'cron', hour=0)
+# Instantiate the scheduler
+scheduler = BackgroundScheduler()
+
+# Add a job to run at 12:00 AM every day
+scheduler.add_job(reset_day_count, 'cron', hour=0)
+
+# Start the scheduler
 scheduler.start()
 
 mbti_list = [
@@ -35,7 +43,7 @@ mbti_list = [
 ]
 
 # 카카오 챗봇 callback API
-async def create_callback_request_kakao(prompt: str, url: str, db: Session):
+async def create_callback_request_kakao(prompt: str, url: str, user_id: str, db: Session):
     '''
     카카오 챗봇 callback 함수
 
@@ -66,7 +74,15 @@ async def create_callback_request_kakao(prompt: str, url: str, db: Session):
             checklist="checklist",
             is_public=True,
         )
-        await createDiary(create, 2, db)
+        diary = await createDiary(create, 2, db)
+
+        # kakao_user_dream 생성
+        dream = kakao_chatbot_dream(
+            kakao_user_id=user_id,
+            dream_id=diary.id,
+        )
+        db.add(dream)
+        db.commit()
 
         # 카카오 챗봇 응답
         outputs = [
@@ -79,11 +95,16 @@ async def create_callback_request_kakao(prompt: str, url: str, db: Session):
         ).dict()
         response = requests.post(url, json=request_body)
 
+        user = db.query(kakao_chatbot_user).filter(kakao_chatbot_user.kakao_user_id == user_id).first()
+        user.day_count += 1
+        user.total_generated_dream += 1
+        db.commit()
+
         # 카카오 챗봇 응답 확인
         if response.status_code == 200:
             print("kakao chatbot callback request success")
         else:
-            print("kakao chatbot callback request failed")
+            print(f"kakao chatbot callback request fail: {response.status_code}, {response.text}")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -105,29 +126,50 @@ async def make_chatgpt_async_callback_request_to_openai_from_kakao(
     '''
     # user_id는 카카오 챗봇 사용자의 고유 식별자입니다.
     user_id = kakao_ai_request['userRequest']['user']['id']
-    print("user_id : ", user_id)
-    print("user_properties_id : ", kakao_ai_request['userRequest']['user']['properties']['botUserKey'])
-    # user_requests는 각 사용자의 요청 횟수를 추적하기 위해 사용됩니다.
-    # user_id가 user_requests에 없으면 0으로 초기화합니다.
-    if user_id not in user_requests:
-        user_requests[user_id] = 0
+
+    # database에 저장된 사용자의 정보를 가져옵니다.
+    user = db.query(kakao_chatbot_user).filter(kakao_chatbot_user.kakao_user_id == user_id).first()
+    if user is None:
+        user = kakao_chatbot_user(
+            kakao_user_id=user_id,
+            day_count=0,
+            total_generated_dream=0,
+        )
+        db.add(user)
+        db.commit()
 
     # 요청이 하루에 3회를 초과하면 에러 메시지를 반환합니다.
-    if user_requests[user_id] >= MAX_REQUESTS_PER_DAY:
+    if user.day_count >= MAX_REQUESTS_PER_DAY:
         return {"version": "2.0",
                 "template": {"outputs": [{"simpleText": {"text": "꿈 분석은 하루에 3번만 가능해요ㅠㅠ 내일 다시 시도해주세요"}}]}}
 
-    # 텍스트의 길이가 10자 이상 200자 이하인지 확인합니다. 만약 그렇지 않으면 에러 메시지를 반환합니다.
-    if len(kakao_ai_request['userRequest']['utterance']) < 10 or len(
+    # mbti 설정하기
+    if kakao_ai_request['userRequest']['utterance'].lower() in mbti_list:
+        user.mbti = kakao_ai_request['userRequest']['utterance'].lower()
+        db.commit()
+        return {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": "mbti를 " + user.mbti + "로 설정했어요!"}}]}}
+
+    # 도움말 보여주기
+    elif len(kakao_ai_request['userRequest']['utterance']) == "도움말":
+        return {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": "mbti를 설정하면 mbti별 꿈 해몽을 해드려요!\n\n mbti를 설정하려면 mbti를 입력해주세요!\n\n 꿈은 10자 이상 200자 이하로 입력해주세요"}}]}}
+
+    # 내 정보 보여주기
+    elif len(kakao_ai_request['userRequest']['utterance']) == "내 정보":
+        return {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": "내 mbti: " + user.mbti + "\n오늘 남은 요청 횟수: " + str(MAX_REQUESTS_PER_DAY - user.day_count) + "번\n총 생성한 꿈의 수: " + str(user.total_generated_dream) + "개"}}]}}
+
+    # 꿈 해몽하기
+    elif len(kakao_ai_request['userRequest']['utterance']) < 10 or len(
             kakao_ai_request['userRequest']['utterance']) > 200:
         return {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": "꿈은 10자 이상 200자 이하로 입력해주세요"}}]}}
 
+    # 무의식 분석
+    elif len(kakao_ai_request['userRequest']['utterance']) == "무의식 분석":
+        return {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": "무의식 분석은 기능은 준비 중입니다"}}]}}
+
     # 백그라운드에서 create_callback_request_kakao 함수를 실행하여 카카오 챗봇에게 응답을 보냅니다.
     background_tasks.add_task(create_callback_request_kakao,
-                              prompt=kakao_ai_request['userRequest']['utterance'],
-                              url=kakao_ai_request['userRequest']['callbackUrl'], db=db)
+                              prompt=user.mbti + ", " + kakao_ai_request['userRequest']['utterance'],
+                              url=kakao_ai_request['userRequest']['callbackUrl'], user_id=user_id, db=db)
 
-    # 요청 횟수를 1회 증가시킵니다.
-    user_requests[user_id] += 1
     # 카카오 챗봇에게 보낼 응답을 반환합니다.
     return {"version": "2.0", "useCallback": True}
