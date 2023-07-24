68be0693c85c5f2fdc9b153ece5cba7090018284
taewan <taewan2002@naver.com>
2023-07-24 19:26:04 +0900

    fix: db 수정

--- app/db/models/mbti_data_KR.py
+++ app/db/models/mbti_data_KR.py
@@ -3,11 +3,11 @@ from app.db.models.diary_jp import get_Diary_jpBase
 
 Base = get_Diary_jpBase()
 
-class Mbti_data(Base):
-    __tablename__ = "MBTI_data"
+class Mbti_data_KR(Base):
+    __tablename__ = "MBTI_data_KR"
 
     id = Column(Integer, primary_key=True)
     user_text = Column(Text, nullable=False)
     mbti_resolution = Column(Text, nullable=False)
-def get_MBTIBase():
+def get_Base():
     return Base
