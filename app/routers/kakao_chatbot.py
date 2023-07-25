820b13a8fc2a11e9f52f205c9bb91c827ba4c3e4
taewan <taewan2002@naver.com>
2023-07-25 16:53:26 +0900

    feat: 심리상태 점수 기능 추가

--- app/routers/kakao_chatbot.py
+++ app/routers/kakao_chatbot.py
@@ -298,9 +298,9 @@ async def kakao_ai_chatbot_callback(
                 my_dream = db.query(Diary_ko).filter(Diary_ko.id == diary_id).first()
                 return {"version": "2.0", "template": {"outputs": [{"SimpleImage": {"imageUrl": my_dream_url.image_url}}, {"simpleText": {"text": f"{my_dream.dream_name}\n\n꿈 내용: {my_dream.dream}\n\n해몽: {my_dream.dream_resolution}"}}]}}
         except:
-            print("diary_id: " + diary_id)
-            print("my_dream_url: " + my_dream_url)
-            print("my_dream: " + my_dream)
+            print("diary_id: " + str(diary_id))
+            print("my_dream_url: " + str(my_dream_url))
+            print("my_dream: " + str(my_dream))
             return {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": "잘못된 입력입니다!"}}]}}
 
     # 무의식 분석
