fd45519e730870adc7d1c14d96269c19e6066f9e
taewan <taewan2002@naver.com>
2023-09-29 15:45:09 +0900

    fix: diary 리스트 조회 기능 수정

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
@@ -11,14 +13,17 @@ router = APIRouter(prefix="/admin")
 
 @router.get("/user/info", response_model=ApiResponse, tags=["Admin"])
 async def get_user_info(
-    user_id: int,
+    user_id: Optional[int] = None,
     current_user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
+    if not user_id:
+        user_id = current_user.id
+
     if current_user.is_admin == True or current_user.id == user_id:
         user = db.query(User).filter(User.id == user_id).first()
-        user.hashed_password = None
         if user:
+            user.hashed_password = None
             return ApiResponse(data=user)
         else:
             raise HTTPException(
