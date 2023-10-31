ffd1a28f15023863516d84f0169d22d1f75dfb82
taewan <taewan2002@naver.com>
2023-10-31 14:29:19 +0900

    fix: database 유저 정보 수정

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from app.db.database import get_Base
+from app.db.models import get_Base
 
 from alembic import context
 
