005694e6c71f87ef1953d413e4588cc8fefa8314
taewan <taewan2002@naver.com>
2023-11-25 22:45:57 +0900

    fix: timeout 예외처리

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app.core.handler import register_exception_handlers, TimeoutMiddleware
+from app.core.handler import register_exception_handlers
 from fastapi import FastAPI
 from app.routers import auth, report, diary, today, admin, chat
 from app.core.middleware import TimingMiddleware
@@ -19,9 +19,8 @@ app.include_router(chat.router)
 app.include_router(report.router)
 app.include_router(diary.router)
 app.include_router(admin.router)
-app.add_middleware(TimingMiddleware)
 register_exception_handlers(app)
-app.add_middleware(TimeoutMiddleware)
+app.add_middleware(TimingMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
