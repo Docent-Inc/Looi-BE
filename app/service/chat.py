531fc101ea62b6b000a9b2b91055f3a47aabb132
taewan <taewan2002@naver.com>
2023-12-25 12:52:54 +0900

    feat: redis 캐싱 적용

--- app/service/chat.py
+++ app/service/chat.py
@@ -100,7 +100,7 @@ class ChatService(AbstractChatService):
 
         # 랜덤으로 하나 선택
         random_chat = random.choice(data)
-        random_chat.text = random_chat.text.replace("{}", self.user.nickname)
+        random_chat = random_chat.text.replace("{}", self.user.nickname)
 
         return random_chat
 
